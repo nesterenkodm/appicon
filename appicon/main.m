87193c9805a3ba2cd37bdedd2e8a1654f54832d9
Dmitry Nesterenko <chebur.mail@gmail.com>
2013-10-06 17:28:27 +0400

    Backup original image if passed corresponding option

--- appicon/main.m
+++ appicon/main.m
@@ -34,8 +34,27 @@ NSString *AIBundleVersionFromInfoPlistFileAtPath(NSString *plist)
     return data[(NSString *)kCFBundleVersionKey];
 }
 
-BOOL AIBurnTextOnImageAtPath(NSString *text, NSString *imagePath)
+typedef NS_ENUM(NSInteger, AIBurnTextOnImageOption) {
+    AIBurnTextOnImageOptionUseBackupCopy = 1 // backup original image if not have been backuped already. And use backuped copy for image processing
+};
+
+BOOL AIBurnTextOnImageAtPath(NSString *text, NSString *imagePath, AIBurnTextOnImageOption options)
 {
+    NSImage *image;
+    if (options & AIBurnTextOnImageOptionUseBackupCopy) {
+        NSString *lastPathComponent = [NSString stringWithFormat:@".%@", imagePath.lastPathComponent];
+        NSString *backupPath = [imagePath.stringByDeletingLastPathComponent stringByAppendingPathComponent:lastPathComponent];
+        if (![[NSFileManager defaultManager] isReadableFileAtPath:backupPath]) {
+            NSError *error;
+            __unused BOOL result = [[NSFileManager defaultManager] copyItemAtPath:imagePath toPath:backupPath error:&error];
+            NSCAssert(result, @"%@", error);
+        }
+        image = [[NSImage alloc] initWithContentsOfFile:backupPath];
+        
+    } else
+        image = [[NSImage alloc] initWithContentsOfFile:imagePath];
+
+    
     NSShadow *shadow = [NSShadow new];
     shadow.shadowOffset = CGSizeMake(0.5, -0.5);
     shadow.shadowColor = [NSColor colorWithCalibratedWhite:0.0 alpha:0.3];
@@ -43,7 +62,6 @@ BOOL AIBurnTextOnImageAtPath(NSString *text, NSString *imagePath)
                                  NSFontAttributeName: [NSFont fontWithName:@"Menlo" size:6],
                                  NSShadowAttributeName: shadow};
     NSSize textSize = [text sizeWithAttributes:attributes];
-    NSImage *image = [[NSImage alloc] initWithContentsOfFile:imagePath];
     [image drawText:text withAttributes:attributes inRect:NSMakeRect(image.size.width / 2.0 - textSize.width / 2.0, - image.size.height + textSize.height + 3, image.size.width, image.size.height)];
     
     return [image writeUsingImageType:NSPNGFileType toFile:imagePath];
@@ -61,7 +79,9 @@ int main(int argc, const char * argv[])
 
         [appIcons enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
             NSLog(@"Burning \"%@\" to the image named %@", version, [obj lastPathComponent]);
-            BOOL result = AIBurnTextOnImageAtPath(version, obj);
+            BOOL result = AIBurnTextOnImageAtPath(version, obj, AIBurnTextOnImageOptionUseBackupCopy);
+            NSCAssert(result, @"Can't burn text on image at path %@", obj);
+            
             *stop = result == NO;
         }];
     }
